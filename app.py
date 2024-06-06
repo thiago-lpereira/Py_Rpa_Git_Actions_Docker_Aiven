f432fe234124d380f39e7ba4be5606bb55e6903d
thiago-lpereira <124629680+thiago-lpereira@users.noreply.github.com>
2024-06-06 09:23:39 -0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-
-def devops():
-    return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS</center>'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
