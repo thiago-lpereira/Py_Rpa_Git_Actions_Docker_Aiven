b09b5e9847ef0da266b19d9b4cf03f7b7abcbbc6
thiago-lpereira <124629680+thiago-lpereira@users.noreply.github.com>
2024-06-06 00:01:32 -0300

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,15 +1,14 @@
 import unittest
-from app import app
+from DolarCotacaoInsertPostgresPyinstall import DolarCotacaoInsertPostgresPyinstall
 
 class FlaskAppTests(unittest.TestCase):
 
     def setUp(self):
-        self.client = app.test_client()
+        self.client = DolarCotacaoInsertPostgresPyinstall.test_client()
 
     def test_devops(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Germinare',response.data)
     
 if __name__ == '__main__':
  unittest.main()
